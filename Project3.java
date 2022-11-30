58cb763934aa05e6c0b26565c9bebc232331f46f
Edwin Argueta <46907039+edwinargueta1@users.noreply.github.com>
2022-11-30 03:57:16 -0800

    Final Upload
    Bisectional Search works as intended on a 2D Array.

--- Project3.java
+++ Project3.java
@@ -1,3 +1,7 @@
+//Name: Edwin Argueta
+//Class: Comp 482 Algorithm Design
+//Assignment: Project 3
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -5,19 +9,55 @@ import java.io.FileNotFoundException;
 public class Project3{
     
     //Global Variables
-    static int target, row, col;
-    static int table[][];
+    static int table[][], target, colLow, rowLow, colHigh, rowHigh;
+    static boolean found = false;
 
     public static void main(String args[]) throws FileNotFoundException{
         //Input File taking the data from
         File text = new File("input.txt");
         scan(text);
         printTable();
+
+        //Offsetting to fit coordinates for Array.
+        colHigh -= 1;
+        rowHigh -= 1;
+
+        //Performing BisectionalSearch on matrix
+        bisectionalSearch(colLow, rowLow, colHigh, rowHigh);
+
+        //if the value is not found then give a not found message to the console.
+        if(found == false){
+            System.out.println("\nValue was not found.");
+        }
     }
 
     //Recursive algorithm that searches for a target value in a 2d array.
-    public static void bisectionalSearch(int table[][], int t){
-        
+    public static void bisectionalSearch(int colL, int rowL, int colH, int rowH){
+
+        //Finds the middle value
+        int colMiddle =  (colL + colH)/2;
+        int rowMiddle = (rowH + rowL)/2;
+        int middle = table[rowMiddle][colMiddle];
+
+        //Only runs if the number is not found and if it is in bounds of search area.
+        if(found == false && !(colL > colH || rowH < rowL)){
+            //Compares the middle with the target value
+            if(middle == target){
+                System.out.println("\nValue was found at: (" + (colMiddle + 1) + ", " + (rowMiddle + 1) + ")");
+                found = true;
+            }else if(target < middle){ //bottomRight removed
+                bisectionalSearch(colMiddle, rowL, colH, rowMiddle - 1); //topRight
+                bisectionalSearch(colL, rowL, colMiddle - 1, rowMiddle - 1); //topLeft
+                bisectionalSearch(colL, rowMiddle, colMiddle - 1, rowH); //bottomLeft
+            }else if(target > middle){//topLeft removed
+                bisectionalSearch(colMiddle + 1, rowL, colH, rowMiddle); //topRight
+                bisectionalSearch(colMiddle + 1, rowMiddle + 1, colH, rowH); //bottomRight
+                bisectionalSearch(colL, rowMiddle + 1, colMiddle, rowH); //bottomLeft
+            }else{
+                //Error Handling.
+                System.out.println("Something went wrong.");
+            }
+        }
     }
 
 
@@ -25,30 +65,30 @@ public class Project3{
     public static void scan(File input) throws FileNotFoundException{
         Scanner in = new Scanner(input);
         //Getting Size of table
-        row = in.nextInt();
-        col = in.nextInt();
-        pVar(row);
-        pVar(col);
+        rowHigh = in.nextInt();
+        colHigh = in.nextInt();
         target = in.nextInt();
-        table = new int[row][col];
+        //Printing
+        System.out.println(rowHigh + " " + colHigh);
+        System.out.println(target);
+        table = new int[rowHigh][colHigh];
 
-        //adding data to table
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
+        //Adding data to table
+        for(int i = 0; i < rowHigh; i++){
+            for(int j = 0; j < colHigh; j++){
                 table[i][j] = in.nextInt();
             }
         }
+        in.close();
     }
 
+    //Prints out the table
     public static void printTable(){
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
+        for(int i = 0; i < rowHigh; i++){
+            for(int j = 0; j < colHigh; j++){
                 System.out.print(table[i][j] + " ");
             }
             System.out.println();
         }
-    }
-    public static void pVar(int var){
-        System.out.println(var);
     }
 }
